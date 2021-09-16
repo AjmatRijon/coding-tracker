3c4570eca304cb903da47df88872db69e9ed2ec5
AjmatRijon <ajmatullah48@gmail.com>
2021-09-16 11:39:09 +0600

    color chnage with button click

--- app.js
+++ app.js
@@ -3,20 +3,23 @@ let hour = 0;
 let sec = 0;
 document.getElementById('btn').addEventListener('click', () => {
     time();
+    bg();
     document.getElementById('btn-stop').setAttribute("class", "show btn-style")
     document.getElementById('btn').setAttribute("class", "display")
 });
 document.getElementById('btn-stop').addEventListener('click', () => {
     clearInterval(wow);
-    clearInterval(intId);
+    clearInterval(refresch);
     document.getElementById('btn-stop').setAttribute("class", "display")
     document.getElementById('btn').setAttribute("class", "show btn-style")
 });
-let refresch = () => {
-    document.getElementById('head').style = 'color: #' +
-        Math.random().toString(16).slice(-6);
+
+function bg() {
+    refresch = setInterval(() => {
+        document.getElementById('head').style = 'color: #' +
+            Math.random().toString(16).slice(-6);
+    }, 1500)
 }
-let intId = setInterval(refresch, 1500);
 
 function time() {
     wow = setInterval(() => {
